db7e1b0096f2b17cbb6f4bfbb9cf91a9f7899f47
JBuenno <joaopedroguedesbueno@aluno.itj.g12.br>
2026-06-18 08:49:31 -0300

    Add files via upload

---
+++ main/java/DAO/EquipeDAOInterface.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+import entidade.Equipe;
+
+public interface EquipeDAOInterface {
+
+    void inserir(Equipe equipe) throws SQLException;
+
+    List<Equipe> listar() throws SQLException;
+
+    Equipe buscarPorId(int id) throws SQLException;
+
+    void atualizar(Equipe equipe) throws SQLException;
+
+    void deletar(int id) throws SQLException;
+}
